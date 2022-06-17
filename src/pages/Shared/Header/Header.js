2fba59c18f94fd80921e17692e2e4ad2bc8d6002
fname lname <monzurulislam997@gmail.com>
2022-06-17 21:57:19 +0600

    header and not found page implemented

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-
-        </div>
+        <nav>
+            <Link to="/home" >Home</Link>
+            <Link to="/service" >Service</Link>
+            <Link to="/blogs" >Blogs</Link>
+            <Link to="/about" >About Me</Link>
+        </nav>
     );
 };
 
